09760c22bfb51fd4d4c0ba791edcf0fe9b408f8d
amontalvof <amontalvof92@gmail.com>
2023-01-18 00:26:02 -0600

    feat: Restocking Cakes

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const redux = require('redux');
 const { createStore } = redux;
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
 
 function orderCake() {
     return {
         type: CAKE_ORDERED,
-        quantity: 1,
+        payload: 1,
+    };
+}
+
+function restockCake(qty = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty,
     };
 }
 
@@ -20,7 +28,12 @@ const reducer = (state = initialState, action) => {
         case CAKE_ORDERED:
             return {
                 ...state,
-                numberOfCakes: state.numberOfCakes - action.quantity,
+                numberOfCakes: state.numberOfCakes - action.payload,
+            };
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numberOfCakes: state.numberOfCakes + action.payload,
             };
         default:
             return state;
@@ -35,5 +48,6 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 unsubscribe();
 store.dispatch(orderCake());
